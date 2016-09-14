c708c3a7cd5d1fe5c1e8640c079c196de63067eb
Saad <saadodi44@gmail.com>
2016-09-14 13:49:26 +0300

    oh god this is bad

--- src/main/java/com/teamwizardry/refraction/common/effect/EffectAttract.java
+++ src/main/java/com/teamwizardry/refraction/common/effect/EffectAttract.java
@@ -1,11 +1,9 @@
 package com.teamwizardry.refraction.common.effect;
 
-import com.teamwizardry.refraction.Refraction;
 import com.teamwizardry.refraction.api.Effect;
-import com.teamwizardry.refraction.client.fx.SparkleFX;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
@@ -13,7 +11,6 @@ import net.minecraft.world.World;
 
 import java.awt.*;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by LordSaad44
@@ -26,11 +23,12 @@ public class EffectAttract extends Effect {
 	}
 
 	private void setEntityMotion(Entity entity) {
-		Vec3d pullDir = beam.initLoc.subtract(beam.finalLoc).normalize();
+		Vec3d pullDir = beam.initLoc.subtract(beam.finalLoc);
+		if (pullDir.lengthVector() > 1) pullDir.normalize();
 
-		entity.motionX = pullDir.xCoord * potency / 1000;
-		entity.motionY = pullDir.yCoord * potency / 1000;
-		entity.motionZ = pullDir.zCoord * potency / 1000;
+		entity.motionX = pullDir.xCoord * potency / 30000;
+		entity.motionY = pullDir.yCoord * potency / 30000;
+		entity.motionZ = pullDir.zCoord * potency / 30000;
 	}
 
 	@Override
@@ -38,9 +36,14 @@ public class EffectAttract extends Effect {
 		int potency = this.potency * 3 / 64;
 		BlockPos block = new BlockPos(pos);
 		AxisAlignedBB axis = new AxisAlignedBB(block, block.add(1, 1, 1));
-		List<Entity> entities = world.getEntitiesWithinAABB(EntityItem.class, axis);
+		List<Entity> entities = world.getEntitiesWithinAABB(Entity.class, axis);
 		if (potency > 128)
 			entities.addAll(world.getEntitiesWithinAABB(EntityLiving.class, axis));
+		EntityPlayer player = world.getClosestPlayer(pos.xCoord, pos.yCoord, pos.zCoord, 1, false);
+		if (player != null) {
+			setEntityMotion(player);
+			player.velocityChanged = true;
+		}
 
 		int pulled = 0;
 		for (Entity entity : entities) {
@@ -48,23 +51,6 @@ public class EffectAttract extends Effect {
 			if (pulled > 200)
 				break;
 			setEntityMotion(entity);
-		}
-
-		for (int i = 0; i < 5; i++) {
-			Vec3d position = new Vec3d(pos.xCoord + ThreadLocalRandom.current().nextDouble(-0.5, 0.5), pos.yCoord + ThreadLocalRandom.current().nextDouble(-0.5, 0.5), pos.zCoord + ThreadLocalRandom.current().nextDouble(-0.5, 0.5));
-			Vec3d motion = pos.subtract(position).scale(1 / 2);
-
-			SparkleFX fx = Refraction.proxy.spawnParticleSparkle(world, position.xCoord, position.yCoord, position.zCoord);
-			fx.blur();
-			fx.setAlpha(0.3f);
-			fx.setScale(0.5f);
-			fx.setAge(30);
-			fx.fadeIn();
-			fx.fadeOut();
-			if (ThreadLocalRandom.current().nextBoolean())
-				fx.blur();
-			fx.setColor(new Color(0x00008B));
-			fx.setMotion(motion);
 		}
 	}
 
