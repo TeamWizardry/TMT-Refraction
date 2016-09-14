ad6a9f0b6a6eeb9937e2d4209f91bfaab43ccfb2
Saad <saadodi44@gmail.com>
2016-09-14 23:13:54 +0300

    alpha bar for spectrometer

--- src/main/java/com/teamwizardry/refraction/client/render/RenderSpectrometer.java
+++ src/main/java/com/teamwizardry/refraction/client/render/RenderSpectrometer.java
@@ -24,25 +24,27 @@ public class RenderSpectrometer extends TileEntitySpecialRenderer<TileSpectromet
 		double psh = 0.1, psw = 1;
 		float transparency = te.getTransparency();
 
-		double r = 0;
-		double g = 0;
-		double b = 0;
+		double r = 0, g = 0, b = 0, a = 0;
 		if (te.getColor() != null) {
 			r = te.getColor().getRed() / 255.0;
 			g = te.getColor().getGreen() / 255.0;
 			b = te.getColor().getBlue() / 255.0;
+			a = te.getTransparency() / 255.0;
 		}
 		if (value == EnumFacing.EAST || value == EnumFacing.WEST) {
 			r *= -1;
 			g *= -1;
 			b *= -1;
+			a *= -1;
 			if (r > -0.5) r -= psh;
 			if (g > -0.5) g -= psh;
 			if (b > -0.5) b -= psh;
+			if (a > -0.5) a -= psh;
 		} else {
 			if (r > 0.5) r -= psh;
 			if (g > 0.5) g -= psh;
 			if (b > 0.5) b -= psh;
+			if (a > 0.5) a -= psh;
 		}
 		// RED //
 		GlStateManager.pushMatrix();
@@ -178,5 +180,50 @@ public class RenderSpectrometer extends TileEntitySpecialRenderer<TileSpectromet
 		GlStateManager.disableBlend();
 		GlStateManager.popMatrix();
 		// BLUE //
+
+		// ALPHA //
+		GlStateManager.pushMatrix();
+		GlStateManager.enableBlend();
+		GlStateManager.color(1, 1, 1, transparency);
+		GlStateManager.translate(x, y + 1.5, z + 0.5);
+		switch (value) {
+			case NORTH:
+				GlStateManager.rotate(270, 1, 0, 0);
+				GlStateManager.translate(0, 0.51, -1 + a);
+				break;
+			case SOUTH:
+				GlStateManager.rotate(270, 1, 0, 0);
+				GlStateManager.translate(0, -0.51, -1 + a);
+				break;
+			case EAST:
+				GlStateManager.rotate(270, 0, 0, 1);
+				GlStateManager.rotate(90, 0, 1, 0);
+				GlStateManager.translate(-0.5, 1.01, 2 + a);
+				break;
+			case WEST:
+				GlStateManager.rotate(270, 0, 0, 1);
+				GlStateManager.rotate(90, 0, 1, 0);
+				GlStateManager.translate(-0.5, -0.01, 2 + a);
+				break;
+		}
+		GlStateManager.translate(0, 0, -0.5);
+
+		GlStateManager.disableCull();
+		GlStateManager.disableLighting();
+
+		Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation(Refraction.MOD_ID, "textures/bar.png"));
+
+		buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
+		buffer.pos(psw, 0, psh).tex(0, 0).endVertex();
+		buffer.pos(0, 0, psh).tex(0, 1).endVertex();
+		buffer.pos(0, 0, 0).tex(1, 1).endVertex();
+		buffer.pos(psw, 0, 0).tex(1, 0).endVertex();
+		Tessellator.getInstance().draw();
+
+		GlStateManager.enableLighting();
+		GlStateManager.enableCull();
+		GlStateManager.disableBlend();
+		GlStateManager.popMatrix();
+		// ALPHA //
 	}
 }
