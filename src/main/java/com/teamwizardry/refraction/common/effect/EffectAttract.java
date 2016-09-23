5238f57901e26b149f60bee987494bd627fd6cb9
Saad <saadodi44@gmail.com>
2016-09-23 11:26:38 +0300

    better disperse and attract effects

--- src/main/java/com/teamwizardry/refraction/common/effect/EffectAttract.java
+++ src/main/java/com/teamwizardry/refraction/common/effect/EffectAttract.java
@@ -1,9 +1,6 @@
 package com.teamwizardry.refraction.common.effect;
 
-import java.awt.Color;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import com.teamwizardry.refraction.api.Effect;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
@@ -11,43 +8,41 @@ import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import com.teamwizardry.refraction.api.Effect;
+
+import java.awt.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by LordSaad44
  */
-public class EffectAttract extends Effect
-{
+public class EffectAttract extends Effect {
 
 	@Override
-	public EffectType getType()
-	{
+	public EffectType getType() {
 		return EffectType.BEAM;
 	}
 
-	private void setEntityMotion(Entity entity)
-	{
+	private void setEntityMotion(Entity entity) {
 		Vec3d pullDir = beam.initLoc.subtract(beam.finalLoc).normalize();
 
 		entity.motionX = pullDir.xCoord * potency / 255;
-		entity.motionY = pullDir.yCoord * potency / 255;
+		entity.motionY = Math.max(-0.25, pullDir.yCoord * potency / 255);
 		entity.motionZ = pullDir.zCoord * potency / 255;
+		entity.fallDistance = 0;
 	}
 
 	@Override
-	public void run(World world, Set<BlockPos> locations)
-	{
-		int potency = this.potency * 3 / 64;
+	public void run(World world, Set<BlockPos> locations) {
 		Set<Entity> toPull = new HashSet<>();
-		for (BlockPos pos : locations)
-		{
+		for (BlockPos pos : locations) {
 			AxisAlignedBB axis = new AxisAlignedBB(pos);
 			List<Entity> entities = world.getEntitiesWithinAABB(Entity.class, axis);
 			if (potency > 128)
 				entities.addAll(world.getEntitiesWithinAABB(EntityLiving.class, axis));
 			EntityPlayer player = world.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), 1, false);
-			if (player != null)
-			{
+			if (player != null) {
 				setEntityMotion(player);
 				player.velocityChanged = true;
 			}
@@ -55,8 +50,7 @@ public class EffectAttract extends Effect
 		}
 
 		int pulled = 0;
-		for (Entity entity : toPull)
-		{
+		for (Entity entity : toPull) {
 			pulled++;
 			if (pulled > 200)
 				break;
@@ -67,8 +61,7 @@ public class EffectAttract extends Effect
 	}
 
 	@Override
-	public Color getColor()
-	{
+	public Color getColor() {
 		return Color.CYAN;
 	}
 }
--- src/main/java/com/teamwizardry/refraction/common/effect/EffectDisperse.java
+++ src/main/java/com/teamwizardry/refraction/common/effect/EffectDisperse.java
@@ -1,84 +1,68 @@
 package com.teamwizardry.refraction.common.effect;
 
-import java.awt.Color;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import com.teamwizardry.refraction.api.Effect;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import com.teamwizardry.refraction.api.Effect;
+
+import java.awt.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by LordSaad44
  */
-public class EffectDisperse extends Effect
-{
+public class EffectDisperse extends Effect {
+
 	@Override
-	public EffectType getType()
-	{
+	public EffectType getType() {
 		return EffectType.BEAM;
 	}
 
-	private void setEntityMotion(Entity entity)
-	{
+	private void setEntityMotion(Entity entity) {
 		Vec3d pullDir = beam.finalLoc.subtract(beam.initLoc).normalize();
 
-		entity.motionX = pullDir.xCoord * potency;
-		entity.motionY = pullDir.yCoord * potency;
-		entity.motionZ = pullDir.zCoord * potency;
+		entity.motionX = pullDir.xCoord * potency / 255;
+		entity.motionY = Math.max(0.3, pullDir.yCoord * potency / 255);
+		entity.motionZ = pullDir.zCoord * potency / 255;
+		entity.fallDistance = 0;
 	}
 
 	@Override
-	public void run(World world, Set<BlockPos> locations)
-	{
+	public void run(World world, Set<BlockPos> locations) {
 		int potency = this.potency * 3 / 64;
 		Set<Entity> toPush = new HashSet<>();
-		for (BlockPos pos : locations)
-		{
+		for (BlockPos pos : locations) {
 			AxisAlignedBB axis = new AxisAlignedBB(pos);
-			List<Entity> entities = world.getEntitiesWithinAABB(EntityItem.class, axis);
+			List<Entity> entities = world.getEntitiesWithinAABB(Entity.class, axis);
 			if (potency > 128)
 				entities.addAll(world.getEntitiesWithinAABB(EntityLiving.class, axis));
+			EntityPlayer player = world.getClosestPlayer(pos.getX(), pos.getY(), pos.getZ(), 1, false);
+			if (player != null) {
+				setEntityMotion(player);
+				player.velocityChanged = true;
+			}
 			toPush.addAll(entities);
 		}
-		
-		if (toPush != null)
-		{
-			int pulled = 0;
-			for (Entity entity : toPush)
-			{
-				pulled++;
-				if (pulled > 200)
-					break;
-				setEntityMotion(entity);
-			}
-		}
 
-		// for (int i = 0; i < 5; i++) {
-		// SparkleFX fx = Refraction.proxy.spawnParticleSparkle(world,
-		// pos.xCoord, pos.yCoord, pos.zCoord);
-		// fx.blur();
-		// fx.setAlpha(0.3f);
-		// fx.setScale(0.5f);
-		// fx.setAge(30);
-		// fx.fadeIn();
-		// fx.fadeOut();
-		// if (ThreadLocalRandom.current().nextBoolean()) fx.blur();
-		// fx.setColor(Color.rgb(0x00008B));
-		// fx.setMotion(new Vec3d(ThreadLocalRandom.current().nextDouble(0.03,
-		// 0.1), ThreadLocalRandom.current().nextDouble(0.03, 0.05),
-		// ThreadLocalRandom.current().nextDouble(0.03, 0.1)));
-		// }
+		int pulled = 0;
+		for (Entity entity : toPush) {
+			pulled++;
+			if (pulled > 200)
+				break;
+			setEntityMotion(entity);
+			if (entity instanceof EntityPlayer)
+				((EntityPlayer) entity).velocityChanged = true;
+		}
 	}
 
 	@Override
-	public Color getColor()
-	{
+	public Color getColor() {
 		return Color.MAGENTA;
 	}
 }
