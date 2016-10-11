470c39cca1a746eb4929a9109063d70cacba9b88
Saad <saadodi44@gmail.com>
2016-10-11 17:42:49 +0300

    almost perfectly functional models except for a badly uv mapped assembly table

--- src/main/java/com/teamwizardry/refraction/client/render/RenderSpectrometer.java
+++ src/main/java/com/teamwizardry/refraction/client/render/RenderSpectrometer.java
@@ -1,14 +1,18 @@
 package com.teamwizardry.refraction.client.render;
 
+import com.teamwizardry.librarianlib.client.core.ClientTickHandler;
+import com.teamwizardry.librarianlib.client.sprite.Sprite;
+import com.teamwizardry.librarianlib.client.sprite.Texture;
+import com.teamwizardry.refraction.Refraction;
 import com.teamwizardry.refraction.common.block.BlockSpectrometer;
 import com.teamwizardry.refraction.common.tile.TileSpectrometer;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.VertexBuffer;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.EnumFacing;
-import org.lwjgl.opengl.GL11;
+import net.minecraft.util.ResourceLocation;
 
 import java.awt.*;
 
@@ -20,59 +24,47 @@ public class RenderSpectrometer extends TileEntitySpecialRenderer<TileSpectromet
 	public void renderTileEntityAt(TileSpectrometer te, double x, double y, double z, float partialTicks, int destroyStage) {
 		VertexBuffer buffer = Tessellator.getInstance().getBuffer();
 		EnumFacing value = te.getWorld().getBlockState(te.getPos()).getValue(BlockSpectrometer.FACING);
-		double psh = 0.1, psw = 0.5;
 
 		te.currentColor = new Color(te.currentColor.getRed() + (int) (Math.ceil((te.maxColor.getRed() - te.currentColor.getRed()) / 100.0)),
 				te.currentColor.getGreen() + (int) (Math.ceil((te.maxColor.getGreen() - te.currentColor.getGreen()) / 100.0)),
 				te.currentColor.getBlue() + (int) (Math.ceil((te.maxColor.getBlue() - te.currentColor.getBlue()) / 100.0)));
 		te.currentTransparency = te.currentTransparency + (int) ((te.maxTransparency - te.currentTransparency) / 100.0);
 
-		double r = (te.currentColor.getRed() / 510.0);
-		double g = (te.currentColor.getGreen() / 510.0);
-		double b = (te.currentColor.getBlue() / 510.0);
-		double a = (te.currentTransparency / 510.0);
-		//Minecraft.getMinecraft().thePlayer.sendChatMessage(te.currentTransparency + "");
+		double r = (te.currentColor.getRed() / 255.0) / 100.0;
+		double g = (te.currentColor.getGreen() / 255.0) / 100.0;
+		double b = (te.currentColor.getBlue() / 255.0) / 100.0;
+		double a = (te.currentTransparency / 255.0) / 100.0;
 
-		if (r > 0.25) r -= psh;
-		if (g > 0.25) g -= psh;
-		if (b > 0.25) b -= psh;
-		if (a > 0.25) a -= psh;
-
-		// RED //
 		GlStateManager.pushMatrix();
-		if (value == EnumFacing.SOUTH || value == EnumFacing.NORTH)
-			GlStateManager.translate(x + 0.5, y + 0.5 + psh + r, z + 0.5);
-		else GlStateManager.translate(x + 0.5, y + 0.5 + r, z + 0.5);
-
-		if (value == EnumFacing.SOUTH) GlStateManager.translate(0, 0, 0.51);
-		else if (value == EnumFacing.NORTH) GlStateManager.translate(-0.5, 0, -0.51);
-		else if (value == EnumFacing.EAST) GlStateManager.translate(0.51, 0, -0.5);
-		else if (value == EnumFacing.WEST) GlStateManager.translate(-0.51, 0, 0);
-
-		GlStateManager.color(1, 0, 0, 1);
 
 		GlStateManager.enableBlend();
 		GlStateManager.disableCull();
 		GlStateManager.disableLighting();
 		GlStateManager.disableTexture2D();
 
-		if (value == EnumFacing.SOUTH || value == EnumFacing.NORTH) {
-			GlStateManager.rotate(90, 1, 0, 0);
-			buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
-			buffer.pos(0, 0, 0).endVertex();
-			buffer.pos(psw, 0, 0).endVertex();
-			buffer.pos(psw, 0, psh).endVertex();
-			buffer.pos(0, 0, psh).endVertex();
-			Tessellator.getInstance().draw();
-		} else {
-			GlStateManager.rotate(90, 0, 0, 1);
-			buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
-			buffer.pos(0, 0, 0).endVertex();
-			buffer.pos(psh, 0, 0).endVertex();
-			buffer.pos(psh, 0, psw).endVertex();
-			buffer.pos(0, 0, psw).endVertex();
-			Tessellator.getInstance().draw();
+		GlStateManager.color(1, 1, 1, 0.2f);
+		GlStateManager.translate(x, y, z + 0.5);
+		switch (value) {
+			case SOUTH:
+				GlStateManager.translate(0, 0, 0.51);
+				break;
+			case NORTH:
+				GlStateManager.translate(0, 0, -0.51);
+				break;
+			case EAST:
+				GlStateManager.rotate(90, 0, 1, 0);
+				GlStateManager.translate(-0.5, 0, 1.01);
+				break;
+			case WEST:
+				GlStateManager.rotate(90, 0, 1, 0);
+				GlStateManager.translate(-0.5, 0, -0.01);
+				break;
 		}
+		ResourceLocation loc = new ResourceLocation(Refraction.MOD_ID, "textures/bar.png");
+		Minecraft.getMinecraft().renderEngine.bindTexture(loc);
+		Texture texture = new Texture(loc);
+		Sprite sprite = texture.getSprite("bar", 1, 1);
+		sprite.draw(ClientTickHandler.getTicks(), 0, 0);
 
 		GlStateManager.enableTexture2D();
 		GlStateManager.enableLighting();
@@ -80,138 +72,5 @@ public class RenderSpectrometer extends TileEntitySpecialRenderer<TileSpectromet
 		GlStateManager.disableBlend();
 
 		GlStateManager.popMatrix();
-		// RED //
-
-		// GREEN //
-		GlStateManager.pushMatrix();
-		if (value == EnumFacing.SOUTH || value == EnumFacing.NORTH)
-			GlStateManager.translate(x + 0.5, y + psh + g, z + 0.5);
-		else GlStateManager.translate(x + 0.5, y + g, z + 0.5);
-
-		if (value == EnumFacing.SOUTH) GlStateManager.translate(0, 0, 0.51);
-		else if (value == EnumFacing.NORTH) GlStateManager.translate(-0.5, 0, -0.51);
-		else if (value == EnumFacing.EAST) GlStateManager.translate(0.51, 0, -0.5);
-		else if (value == EnumFacing.WEST) GlStateManager.translate(-0.51, 0, 0);
-
-		GlStateManager.color(0, 1, 0, 1);
-
-		GlStateManager.enableBlend();
-		GlStateManager.disableCull();
-		GlStateManager.disableLighting();
-		GlStateManager.disableTexture2D();
-
-		if (value == EnumFacing.SOUTH || value == EnumFacing.NORTH) {
-			GlStateManager.rotate(90, 1, 0, 0);
-			buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
-			buffer.pos(0, 0, 0).endVertex();
-			buffer.pos(psw, 0, 0).endVertex();
-			buffer.pos(psw, 0, psh).endVertex();
-			buffer.pos(0, 0, psh).endVertex();
-			Tessellator.getInstance().draw();
-		} else {
-			GlStateManager.rotate(90, 0, 0, 1);
-			buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
-			buffer.pos(0, 0, 0).endVertex();
-			buffer.pos(psh, 0, 0).endVertex();
-			buffer.pos(psh, 0, psw).endVertex();
-			buffer.pos(0, 0, psw).endVertex();
-			Tessellator.getInstance().draw();
-		}
-
-		GlStateManager.enableTexture2D();
-		GlStateManager.enableLighting();
-		GlStateManager.enableCull();
-		GlStateManager.disableBlend();
-
-		GlStateManager.popMatrix();
-		// GREEN //
-
-		// BLUE //
-		GlStateManager.pushMatrix();
-		if (value == EnumFacing.SOUTH || value == EnumFacing.NORTH)
-			GlStateManager.translate(x + 0.5, y + psh + b, z + 0.5);
-		else GlStateManager.translate(x + 0.5, y + b, z + 0.5);
-
-		if (value == EnumFacing.SOUTH) GlStateManager.translate(-0.5, 0, 0.51);
-		else if (value == EnumFacing.NORTH) GlStateManager.translate(0, 0, -0.51);
-		else if (value == EnumFacing.EAST) GlStateManager.translate(0.51, 0, 0);
-		else if (value == EnumFacing.WEST) GlStateManager.translate(-0.51, 0, -0.5);
-
-		GlStateManager.color(0, 0, 1, 1);
-
-		GlStateManager.enableBlend();
-		GlStateManager.disableCull();
-		GlStateManager.disableLighting();
-		GlStateManager.disableTexture2D();
-
-		if (value == EnumFacing.SOUTH || value == EnumFacing.NORTH) {
-			GlStateManager.rotate(90, 1, 0, 0);
-			buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
-			buffer.pos(0, 0, 0).endVertex();
-			buffer.pos(psw, 0, 0).endVertex();
-			buffer.pos(psw, 0, psh).endVertex();
-			buffer.pos(0, 0, psh).endVertex();
-			Tessellator.getInstance().draw();
-		} else {
-			GlStateManager.rotate(90, 0, 0, 1);
-			buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
-			buffer.pos(0, 0, 0).endVertex();
-			buffer.pos(psh, 0, 0).endVertex();
-			buffer.pos(psh, 0, psw).endVertex();
-			buffer.pos(0, 0, psw).endVertex();
-			Tessellator.getInstance().draw();
-		}
-
-		GlStateManager.enableTexture2D();
-		GlStateManager.enableLighting();
-		GlStateManager.enableCull();
-		GlStateManager.disableBlend();
-
-		GlStateManager.popMatrix();
-		// BLUE //
-
-		// ALPHA //
-		GlStateManager.pushMatrix();
-		if (value == EnumFacing.SOUTH || value == EnumFacing.NORTH)
-			GlStateManager.translate(x + 0.5, y + 0.5 + psh + a, z + 0.5);
-		else GlStateManager.translate(x + 0.5, y + 0.5 + a, z + 0.5);
-
-		if (value == EnumFacing.SOUTH) GlStateManager.translate(-0.5, 0, 0.51);
-		else if (value == EnumFacing.NORTH) GlStateManager.translate(0, 0, -0.51);
-		else if (value == EnumFacing.EAST) GlStateManager.translate(0.51, 0, 0);
-		else if (value == EnumFacing.WEST) GlStateManager.translate(-0.51, 0, -0.5);
-
-		GlStateManager.color(1, 1, 1, 1);
-
-		GlStateManager.enableBlend();
-		GlStateManager.disableCull();
-		GlStateManager.disableLighting();
-		GlStateManager.disableTexture2D();
-
-		if (value == EnumFacing.SOUTH || value == EnumFacing.NORTH) {
-			GlStateManager.rotate(90, 1, 0, 0);
-			buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
-			buffer.pos(0, 0, 0).endVertex();
-			buffer.pos(psw, 0, 0).endVertex();
-			buffer.pos(psw, 0, psh).endVertex();
-			buffer.pos(0, 0, psh).endVertex();
-			Tessellator.getInstance().draw();
-		} else {
-			GlStateManager.rotate(90, 0, 0, 1);
-			buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
-			buffer.pos(0, 0, 0).endVertex();
-			buffer.pos(psh, 0, 0).endVertex();
-			buffer.pos(psh, 0, psw).endVertex();
-			buffer.pos(0, 0, psw).endVertex();
-			Tessellator.getInstance().draw();
-		}
-
-		GlStateManager.enableTexture2D();
-		GlStateManager.enableLighting();
-		GlStateManager.enableCull();
-		GlStateManager.disableBlend();
-
-		GlStateManager.popMatrix();
-		// ALPHA //
 	}
 }
