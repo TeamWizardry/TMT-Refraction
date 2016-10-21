d07c751b96efa559066fdbf2f8344671828cb2d2
murapix <smartseth@hotmail.com>
2016-10-20 22:12:02 -0700

    Fixed my divide by 0 error in the Refraction Chamber... woops

--- src/main/java/com/teamwizardry/refraction/common/tile/TileReflectionChamber.java
+++ src/main/java/com/teamwizardry/refraction/common/tile/TileReflectionChamber.java
@@ -52,62 +52,73 @@ public class TileReflectionChamber extends TileEntity implements IBeamHandler {
 	}
 
 	@Override
-	public void handle(Beam... beams) {
-		if (beams.length <= 0) return;
+	public void handle(Beam... beams)
+	{
+		if (beams.length <= 0)
+			return;
 
-		boolean effectBeam = false;
-		boolean aestheticBeam = false;
-		
+		int effectCount = 0;
+		int aestheticCount = 0;
+
 		Vec3d[] angles1 = new Vec3d[beams.length];
 		Vec3d[] angles2 = new Vec3d[beams.length];
 
-		int red1 = 0, red2 = 0;
-		int green1 = 0, green2 = 0;
-		int blue1 = 0, blue2 = 0;
-		int alpha1 = 0, alpha2 = 0;
-		int effectBeams = 0;
-		for (int i = 0; i < beams.length; i++) {
+		int aRed = 0, eRed = 0;
+		int aGreen = 0, eGreen = 0;
+		int aBlue = 0, eBlue = 0;
+		int aAlpha = 0, eAlpha = 0;
+		for (int i = 0; i < beams.length; i++)
+		{
 			Color color = beams[i].color;
-			if (!beams[i].enableEffect) {
-				aestheticBeam = true;
-				red1 += color.getRed();
-				green1 += color.getGreen();
-				blue1 += color.getBlue();
-				alpha1 += color.getAlpha();
+			if (!beams[i].enableEffect)
+			{
+				aRed += color.getRed();
+				aGreen += color.getGreen();
+				aBlue += color.getBlue();
+				aAlpha += color.getAlpha();
+				aestheticCount++;
 
 				angles1[i] = beams[i].finalLoc.subtract(beams[i].initLoc);
-			} else {
-				effectBeam = true;
-				red2 += color.getRed();
-				green2 += color.getGreen();
-				blue2 += color.getBlue();
-				alpha2 += color.getAlpha();
-				effectBeams++;
+			}
+			else
+			{
+				eRed += color.getRed();
+				eGreen += color.getGreen();
+				eBlue += color.getBlue();
+				eAlpha += color.getAlpha();
+				effectCount++;
 
 				angles2[i] = beams[i].finalLoc.subtract(beams[i].initLoc);
 			}
 		}
+		if (aestheticCount > 0)
+		{
+			aRed = Math.min(aRed / aestheticCount, 255);
+			aGreen = Math.min(aGreen / aestheticCount, 255);
+			aBlue = Math.min(aBlue / aestheticCount, 255);
 
-		red1 = Math.min(red1 / (beams.length - effectBeams), 255);
-		green1 = Math.min(green1 / (beams.length - effectBeams), 255);
-		blue1 = Math.min(blue1 / (beams.length - effectBeams), 255);
+			float[] hsbvals1 = Color.RGBtoHSB(aRed, aGreen, aBlue, null);
+			Color color1 = new Color(Color.HSBtoRGB(hsbvals1[0], hsbvals1[1], 1));
+			color1 = new Color(color1.getRed(), color1.getGreen(), color1.getBlue(), Math.min(aAlpha, 255));
 
-		red2 = Math.min(red2 / effectBeams, 255);
-		green2 = Math.min(green2 / effectBeams, 255);
-		blue2 = Math.min(blue2 / effectBeams, 255);
+			Vec3d out1 = RotationHelper.averageDirection(angles1);
+			new Beam(worldObj, new Vec3d(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5), out1, color1, false, false);
+		}
 
-		float[] hsbvals1 = Color.RGBtoHSB(red1, green1, blue1, null);
-		Color color1 = new Color(Color.HSBtoRGB(hsbvals1[0], hsbvals1[1], 1));
-		color1 = new Color(color1.getRed(), color1.getGreen(), color1.getBlue(), Math.min(alpha1, 255));
+		if (effectCount > 0)
+		{
+			eRed = Math.min(eRed / effectCount, 255);
+			eGreen = Math.min(eGreen / effectCount, 255);
+			eBlue = Math.min(eBlue / effectCount, 255);
 
-		float[] hsbvals2 = Color.RGBtoHSB(red2, green2, blue2, null);
-		Color color2 = new Color(Color.HSBtoRGB(hsbvals2[0], hsbvals2[1], 1));
-		color2 = new Color(color2.getRed(), color2.getGreen(), color2.getBlue(), Math.min(alpha2, 255));
+			float[] hsbvals2 = Color.RGBtoHSB(eRed, eGreen, eBlue, null);
+			Color color2 = new Color(Color.HSBtoRGB(hsbvals2[0], hsbvals2[1], 1));
+			color2 = new Color(color2.getRed(), color2.getGreen(), color2.getBlue(), Math.min(eAlpha, 255));
 
-		Vec3d out1 = RotationHelper.averageDirection(angles1);
-		Vec3d out2 = RotationHelper.averageDirection(angles2);
-		if (aestheticBeam) new Beam(worldObj, new Vec3d(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5), out1, color1, false, false);
-		if (effectBeam) new Beam(worldObj, new Vec3d(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5), out2, color2, true, false);
+			Vec3d out2 = RotationHelper.averageDirection(angles2);
+
+			new Beam(worldObj, new Vec3d(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5), out2, color2, true, false);
+		}
 
 	}
 }
