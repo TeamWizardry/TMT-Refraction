448be019f08857ed89c74ae5aaf50dcd03b11b02
yrsegal <yrsegal@gmail.com>
2016-11-01 10:41:35 -0400

    fix translocators accepting from any direction

--- src/main/java/com/teamwizardry/refraction/common/tile/TileTranslocator.java
+++ src/main/java/com/teamwizardry/refraction/common/tile/TileTranslocator.java
@@ -1,8 +1,5 @@
 package com.teamwizardry.refraction.common.tile;
 
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.math.Vec3d;
 import com.teamwizardry.librarianlib.common.base.block.TileMod;
 import com.teamwizardry.refraction.api.ICableHandler;
 import com.teamwizardry.refraction.api.PosUtils;
@@ -10,6 +7,9 @@ import com.teamwizardry.refraction.common.block.BlockTranslocator;
 import com.teamwizardry.refraction.common.light.Beam;
 import com.teamwizardry.refraction.common.light.IBeamHandler;
 import com.teamwizardry.refraction.init.ModBlocks;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.Vec3d;
 
 public class TileTranslocator extends TileMod implements IBeamHandler, ICableHandler
 {
@@ -24,6 +24,8 @@ public class TileTranslocator extends TileMod implements IBeamHandler, ICableHan
 		if (state.getBlock() != ModBlocks.TRANSLOCATOR)
 			beam.spawn();
 		EnumFacing dir = state.getValue(BlockTranslocator.DIRECTION);
+		if (!beam.slope.equals(PosUtils.getVecFromFacing(dir)))
+			return;
 		if (!worldObj.isAirBlock(pos.offset(dir)))
 		{
 			Vec3d slope = beam.slope.normalize().scale(15.0/16.0);
