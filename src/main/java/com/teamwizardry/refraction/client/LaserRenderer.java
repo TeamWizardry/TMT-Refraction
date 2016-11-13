dfcbf1522a3e6d0e5f661864400ea5d38ed293e0
Saad <saadodi44@gmail.com>
2016-11-13 23:05:02 +0200

    moved tps checker to laserRenderer for client tick

--- src/main/java/com/teamwizardry/refraction/client/LaserRenderer.java
+++ src/main/java/com/teamwizardry/refraction/client/LaserRenderer.java
@@ -1,7 +1,6 @@
 package com.teamwizardry.refraction.client;
 
 import com.teamwizardry.refraction.client.render.RenderLaserUtil;
-import com.teamwizardry.refraction.common.core.EventHandler;
 import com.teamwizardry.refraction.common.light.ReflectionTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
@@ -23,7 +22,10 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class LaserRenderer {
 	public static final LaserRenderer INSTANCE = new LaserRenderer();
-	
+
+	private static int currentTick;
+	private static long[] ticks = new long[600];
+
 	protected Map<LaserRenderInfo, Integer> lasers = new ConcurrentHashMap<>();
 
 	private LaserRenderer() {
@@ -31,11 +33,29 @@ public class LaserRenderer {
 	}
 	
 	public static void add(Vec3d start, Vec3d end, Color color) {
-		int life = (int) EventHandler.getTPS() / 20 + 1;
-		//Minecraft.getMinecraft().thePlayer.sendChatMessage(life + "");
+		int life = (int) getTPS() / 20 + 1;
 		INSTANCE.lasers.put(new LaserRenderInfo(start, end, color), life);
 	}
-	
+
+	public static double getTPS() {
+		return Math.max(getTPS(100) / 2, 20);
+	}
+
+	private static double getTPS(int tickCount) {
+		if (tickCount > ticks.length) {
+			tickCount = ticks.length;
+		}
+
+		if (ticks[tickCount] == 0.0) {
+			tickCount = currentTick;
+		}
+
+		int index = ((currentTick - tickCount) + ticks.length) % ticks.length;
+		long elapsed = ticks[currentTick] - ticks[index];
+
+		return (double) tickCount / ((double) elapsed / 1000.0D);
+	}
+
 	@SubscribeEvent
 	public void unload(WorldEvent.Unload event) {
 		lasers.clear();
@@ -74,7 +94,7 @@ public class LaserRenderer {
 		GlStateManager.popMatrix();
 		GlStateManager.popAttrib();
 	}
-	
+
 	@SubscribeEvent
 	public void tick(TickEvent.ClientTickEvent event) {
 		if (event.phase == TickEvent.Phase.START) {
@@ -86,6 +106,10 @@ public class LaserRenderer {
 				}
 			});
 		}
+
+		currentTick = ++currentTick % ticks.length;
+
+		ticks[currentTick] = System.currentTimeMillis();
 	}
 
 	public static class LaserRenderInfo {
--- src/main/java/com/teamwizardry/refraction/common/core/EventHandler.java
+++ src/main/java/com/teamwizardry/refraction/common/core/EventHandler.java
@@ -1,43 +1,14 @@
 package com.teamwizardry.refraction.common.core;
 
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 /**
  * Created by LordSaad44
  */
 public class EventHandler {
 	public static final EventHandler INSTANCE = new EventHandler();
-	private static int currentTick;
-	private static long[] ticks = new long[600];
+
 	private EventHandler() {
 		MinecraftForge.EVENT_BUS.register(this);
-	}
-
-	public static double getTPS() {
-		return Math.max(getTPS(100) / 2, 20);
-	}
-
-	private static double getTPS(int tickCount) {
-		if (tickCount > ticks.length) {
-			tickCount = ticks.length;
-		}
-
-		if (ticks[tickCount] == 0.0) {
-			tickCount = currentTick;
-		}
-
-		int index = ((currentTick - tickCount) + ticks.length) % ticks.length;
-		long elapsed = ticks[currentTick] - ticks[index];
-
-		return (double) tickCount / ((double) elapsed / 1000.0D);
-	}
-
-	@SubscribeEvent
-	public void tick(TickEvent.ServerTickEvent event) {
-		currentTick = ++currentTick % ticks.length;
-
-		ticks[currentTick] = System.currentTimeMillis();
 	}
 }
