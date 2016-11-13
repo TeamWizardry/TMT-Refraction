9a7f636d389696916b6153c23fd1a4a065181a1d
yrsegal <yrsegal@gmail.com>
2016-11-12 21:28:56 -0500

    remove unneeded render extent

--- src/main/java/com/teamwizardry/refraction/common/tile/TileCreativeLaser.java
+++ src/main/java/com/teamwizardry/refraction/common/tile/TileCreativeLaser.java
@@ -3,23 +3,12 @@ package com.teamwizardry.refraction.common.tile;
 import com.teamwizardry.librarianlib.common.base.block.TileMod;
 import com.teamwizardry.librarianlib.common.util.autoregister.TileRegister;
 import com.teamwizardry.refraction.init.ModBlocks;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Created by LordSaad44
  */
 @TileRegister("creative_laser")
 public class TileCreativeLaser extends TileMod {
-
-	@NotNull
-	@SideOnly(Side.CLIENT)
-	@Override
-	public AxisAlignedBB getRenderBoundingBox() {
-		return INFINITE_EXTENT_AABB;
-	}
 
 	@Override
 	public void onLoad() {
