8fbdc468b2eca3967f04eb3684c1107442802b33
Saad <saadodi44@gmail.com>
2016-11-14 20:27:12 +0200

    wormhole particles \o/

--- src/main/java/com/teamwizardry/refraction/common/core/EventHandler.java
+++ src/main/java/com/teamwizardry/refraction/common/core/EventHandler.java
@@ -1,6 +1,9 @@
 package com.teamwizardry.refraction.common.core;
 
+import com.teamwizardry.refraction.common.network.PacketAXYZMarks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 /**
  * Created by LordSaad44
@@ -10,5 +13,15 @@ public class EventHandler {
 
 	private EventHandler() {
 		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@SubscribeEvent
+	public void worldUnload(WorldEvent.Unload event) {
+		PacketAXYZMarks.controlPoints.clear();
+	}
+
+	@SubscribeEvent
+	public void worldLoad(WorldEvent.Load event) {
+		PacketAXYZMarks.controlPoints.clear();
 	}
 }
--- src/main/java/com/teamwizardry/refraction/common/network/PacketAXYZMarks.java
+++ src/main/java/com/teamwizardry/refraction/common/network/PacketAXYZMarks.java
@@ -5,8 +5,11 @@ import com.teamwizardry.librarianlib.client.fx.particle.ParticleSpawner;
 import com.teamwizardry.librarianlib.client.fx.particle.functions.InterpFadeInOut;
 import com.teamwizardry.librarianlib.common.network.PacketBase;
 import com.teamwizardry.librarianlib.common.util.math.interpolate.StaticInterp;
+import com.teamwizardry.librarianlib.common.util.math.interpolate.position.InterpBezier3D;
 import com.teamwizardry.refraction.Refraction;
+import com.teamwizardry.refraction.common.block.BlockAXYZ;
 import io.netty.buffer.ByteBuf;
+import kotlin.Pair;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
@@ -16,9 +19,13 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class PacketAXYZMarks extends PacketBase {
+
+	public static final Map<BlockAXYZ.DimWithPos, Pair<Vec3d, Vec3d>> controlPoints = new HashMap<>();
 
 	private BlockPos[] positions, originPositions;
 	private int dimension;
@@ -41,29 +48,96 @@ public class PacketAXYZMarks extends PacketBase {
 
 		for (int i = 0; i < positions.length; i++) {
 			BlockPos pos = positions[i], origin = originPositions[i];
+			double range = 5;
+			controlPoints.putIfAbsent(new BlockAXYZ.DimWithPos(world.provider.getDimension(), origin),
+					new Pair<>(
+							new Vec3d(ThreadLocalRandom.current().nextDouble(-range, range),
+									ThreadLocalRandom.current().nextDouble(-range, range),
+									ThreadLocalRandom.current().nextDouble(-range, range)),
+							new Vec3d(ThreadLocalRandom.current().nextDouble(-range, range),
+									ThreadLocalRandom.current().nextDouble(-range, range),
+									ThreadLocalRandom.current().nextDouble(-range, range))));
 
-			ParticleBuilder glitter = new ParticleBuilder(20);
-			glitter.setRenderNormalLayer(new ResourceLocation(Refraction.MOD_ID, "particles/glow"));
-			glitter.setAlphaFunction(new InterpFadeInOut(0.3f, 0.3f));
-			ParticleSpawner.spawn(glitter, world, new StaticInterp<>(new Vec3d(pos).addVector(0.5, 0.5, 0.5)), ThreadLocalRandom.current().nextInt(5, 20), 0, (aFloat, particleBuilder) -> {
-				double radius = 0.5;
+
+			ParticleBuilder wormholeVoid = new ParticleBuilder(10);
+			ParticleSpawner.spawn(wormholeVoid, world, new StaticInterp<>(new Vec3d(pos).addVector(0.5, 0.5, 0.5)), ThreadLocalRandom.current().nextInt(10, 20), 0, (aFloat, particleBuilder) -> {
+				wormholeVoid.setAlphaFunction(new InterpFadeInOut(0.2f, 0.2f));
+				wormholeVoid.setRenderNormalLayer(new ResourceLocation(Refraction.MOD_ID, "particles/glow"));
+				double radius = 0.4;
 				double theta = 2.0f * (float) Math.PI * ThreadLocalRandom.current().nextFloat();
 				double r = radius * ThreadLocalRandom.current().nextFloat();
 				double x = r * MathHelper.cos((float) theta);
 				double z = r * MathHelper.sin((float) theta);
-				glitter.setPositionOffset(new Vec3d(x, 0, z));
-				glitter.setColor(new Color(ThreadLocalRandom.current().nextInt(0, 20), 0, ThreadLocalRandom.current().nextInt(0, 20), 255));
-				glitter.setScale(ThreadLocalRandom.current().nextFloat());
-				glitter.addMotion(new Vec3d(0, ThreadLocalRandom.current().nextDouble(0.0005, 0.001), 0));
-				glitter.setLifetime(ThreadLocalRandom.current().nextInt(10, 30));
+				wormholeVoid.setPositionOffset(new Vec3d(x, 0, z));
+				int rnd = ThreadLocalRandom.current().nextInt(0, 40);
+				wormholeVoid.setColor(new Color(rnd, 0, rnd));
+				wormholeVoid.setScale((float) ThreadLocalRandom.current().nextDouble(1, 2));
+				wormholeVoid.setMotion(Vec3d.ZERO);
 			});
 
-			/*ParticleSpawner.spawn(glitter, world, new StaticInterp<>(new Vec3d(pos).addVector(0.5, 0.5, 0.5)), ThreadLocalRandom.current().nextInt(5, 20), 0, (aFloat, particleBuilder) -> {
-				glitter.enableMotionCalculation();
-				glitter.setColor(new Color(ThreadLocalRandom.current().nextInt(0, 10), 0, ThreadLocalRandom.current().nextInt(0, 10), 255));
-				glitter.setScale(ThreadLocalRandom.current().nextFloat());
-				glitter.setLifetime(ThreadLocalRandom.current().nextInt(30, 50));
-			});*/
+			ParticleBuilder wormholeHalo = new ParticleBuilder(15);
+			ParticleSpawner.spawn(wormholeHalo, world, new StaticInterp<>(new Vec3d(pos).addVector(0.5, 0.5, 0.5)), ThreadLocalRandom.current().nextInt(5, 10), 0, (aFloat, particleBuilder) -> {
+				wormholeHalo.setAlphaFunction(new InterpFadeInOut(0.2f, 0.2f));
+				wormholeHalo.setRender(new ResourceLocation(Refraction.MOD_ID, "particles/glow"));
+				double r = 0.5;
+				double theta = 2.0f * (float) Math.PI * ThreadLocalRandom.current().nextFloat();
+				double x = r * MathHelper.cos((float) theta);
+				double z = r * MathHelper.sin((float) theta);
+				wormholeHalo.setPositionOffset(new Vec3d(x, 0, z));
+				wormholeHalo.setColor(new Color(72, 119, 122));
+				wormholeHalo.setScale((float) ThreadLocalRandom.current().nextDouble(1, 2));
+			});
+
+			ParticleBuilder generatorHalo = new ParticleBuilder(10);
+			ParticleSpawner.spawn(generatorHalo, world, new StaticInterp<>(new Vec3d(origin).addVector(0.5, 0.5, 0.5)), ThreadLocalRandom.current().nextInt(5, 10), 0, (aFloat, particleBuilder) -> {
+				generatorHalo.setAlphaFunction(new InterpFadeInOut(0.3f, 0.3f));
+				generatorHalo.setRender(new ResourceLocation(Refraction.MOD_ID, "particles/glow"));
+				double r = 0.5;
+				double theta = 2.0f * (float) Math.PI * ThreadLocalRandom.current().nextFloat();
+				double x = r * MathHelper.cos((float) theta);
+				double z = r * MathHelper.sin((float) theta);
+				generatorHalo.setPositionOffset(new Vec3d(x, 0, z));
+				generatorHalo.setColor(new Color(72, 119, 122));
+				generatorHalo.setScale((float) ThreadLocalRandom.current().nextDouble(1, 2));
+			});
+
+			for (BlockAXYZ.DimWithPos dimWithPos : controlPoints.keySet()) {
+				if (dimWithPos.blockPos.toLong() == origin.toLong()) {
+					if (ThreadLocalRandom.current().nextInt(5) == 0) {
+
+						Pair<Vec3d, Vec3d> pair = controlPoints.get(dimWithPos);
+						double shift = ThreadLocalRandom.current().nextDouble(0.1, 0.5);
+
+						double p1r1 = pair.getFirst().xCoord + ThreadLocalRandom.current().nextDouble(-shift, shift);
+						double p1r2 = pair.getFirst().yCoord + ThreadLocalRandom.current().nextDouble(-shift, shift);
+						double p1r3 = pair.getFirst().zCoord + ThreadLocalRandom.current().nextDouble(-shift, shift);
+						Vec3d p1 = new Vec3d(
+								p1r1 <= 2 ? p1r1 : pair.getFirst().xCoord,
+								p1r2 <= 2 ? p1r2 : pair.getFirst().yCoord,
+								p1r3 <= 2 ? p1r3 : pair.getFirst().zCoord
+						);
+						double p2r1 = pair.getSecond().xCoord + ThreadLocalRandom.current().nextDouble(-shift, shift);
+						double p2r2 = pair.getSecond().yCoord + ThreadLocalRandom.current().nextDouble(-shift, shift);
+						double p2r3 = pair.getSecond().zCoord + ThreadLocalRandom.current().nextDouble(-shift, shift);
+						Vec3d p2 = new Vec3d(
+								p2r1 <= 2 ? p2r1 : pair.getSecond().xCoord,
+								p2r2 <= 2 ? p2r2 : pair.getSecond().yCoord,
+								p2r3 <= 2 ? p2r3 : pair.getSecond().zCoord
+						);
+						controlPoints.put(dimWithPos, new Pair<>(p1, p2));
+
+						ParticleBuilder connection = new ParticleBuilder(30);
+						ParticleSpawner.spawn(connection, world, new StaticInterp<>(new Vec3d(origin).addVector(0.5, 0.5, 0.5)), ThreadLocalRandom.current().nextInt(1, 5), 0, (aFloat, particleBuilder) -> {
+							connection.setAlphaFunction(new InterpFadeInOut(0.2f, 0.2f));
+							connection.setRender(new ResourceLocation(Refraction.MOD_ID, "particles/glow"));
+							Vec3d sub = new Vec3d(pos.subtract(origin));
+							connection.setPositionFunction(new InterpBezier3D(Vec3d.ZERO, sub, controlPoints.get(dimWithPos).getFirst(), controlPoints.get(dimWithPos).getSecond()));
+							connection.setColor(new Color(72, 119, 122));
+							connection.setScale((float) ThreadLocalRandom.current().nextDouble(1, 2));
+						});
+					}
+				}
+			}
 		}
 	}
 
