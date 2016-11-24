1cf39c68de5adebb2b59eb2d7c890ed435c2b5dc
yrsegal <yrsegal@gmail.com>
2016-11-24 14:18:01 -0500

    move nonapi reference out of api

--- src/main/java/com/teamwizardry/refraction/api/book/TextAdapter.java
+++ src/main/java/com/teamwizardry/refraction/api/book/TextAdapter.java
@@ -9,21 +9,10 @@ import com.google.gson.JsonPrimitive;
 import com.google.gson.internal.LazilyParsedNumber;
 import com.teamwizardry.librarianlib.LibrarianLog;
 import com.teamwizardry.librarianlib.common.util.NBTTypes;
-import com.teamwizardry.refraction.client.jei.JEIRefractionPlugin;
-import mezz.jei.api.IRecipeRegistry;
-import mezz.jei.api.gui.IRecipeLayoutDrawable;
-import mezz.jei.api.recipe.IFocus;
-import mezz.jei.api.recipe.IRecipeCategory;
-import mezz.jei.api.recipe.IRecipeWrapper;
-import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
-import net.minecraft.block.Block;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.*;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -327,50 +316,6 @@ public final class TextAdapter {
 		}
 
 		return new StringTextHolder("UNKNOWN DATA TYPE");
-	}
-
-	private static ItemStack getStackFromString(String itemId) {
-		ResourceLocation location = new ResourceLocation(itemId);
-		ItemStack stack = null;
-
-		if (ForgeRegistries.ITEMS.containsKey(location)) {
-			Item item = ForgeRegistries.ITEMS.getValue(location);
-			if (item != null) stack = new ItemStack(item);
-
-		} else if (ForgeRegistries.BLOCKS.containsKey(location)) {
-			Block block = ForgeRegistries.BLOCKS.getValue(location);
-			if (block != null) stack = new ItemStack(block);
-
-		}
-		return stack;
-	}
-
-	private IRecipeLayoutDrawable getDrawableFromItem(String itemId) {
-		ItemStack stack = getStackFromString(itemId);
-
-		if (stack != null) {
-			IRecipeRegistry registry = JEIRefractionPlugin.jeiRuntime.getRecipeRegistry();
-			IFocus<ItemStack> focus = registry.createFocus(IFocus.Mode.OUTPUT, stack);
-			for (IRecipeCategory<?> category : registry.getRecipeCategories(focus)) {
-				if (category.getUid().equals("refraction.assembly_table")
-						|| category.getUid().equals(VanillaRecipeCategoryUid.CRAFTING)) {
-					List<IRecipeLayoutDrawable> layouts = getLayouts(registry, category, focus);
-					if (!layouts.isEmpty())
-						return layouts.get(0);
-				}
-			}
-		}
-		return null;
-	}
-
-	private <T extends IRecipeWrapper> List<IRecipeLayoutDrawable> getLayouts(IRecipeRegistry registry, IRecipeCategory<T> category, IFocus<ItemStack> focus) {
-		List<IRecipeLayoutDrawable> layouts = new ArrayList<>();
-		List<T> wrappers = registry.getRecipeWrappers(category, focus);
-		for (T wrapper : wrappers) {
-			IRecipeLayoutDrawable layout = registry.createRecipeLayoutDrawable(category, wrapper, focus);
-			layouts.add(layout);
-		}
-		return layouts;
 	}
 
 	@FunctionalInterface
