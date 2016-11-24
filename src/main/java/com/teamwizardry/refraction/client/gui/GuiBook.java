fa23bafb0f5eaba3c15dedb98ab93ef4315b14d0
Saad <saadodi44@gmail.com>
2016-11-24 20:24:59 +0200

    scroll view doesnt work right right now

--- src/main/java/com/teamwizardry/refraction/client/gui/GuiBook.java
+++ src/main/java/com/teamwizardry/refraction/client/gui/GuiBook.java
@@ -6,10 +6,10 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.teamwizardry.librarianlib.LibrarianLib;
 import com.teamwizardry.librarianlib.client.gui.GuiBase;
-import com.teamwizardry.librarianlib.client.gui.components.ComponentSprite;
-import com.teamwizardry.librarianlib.client.gui.components.ComponentText;
+import com.teamwizardry.librarianlib.client.gui.components.*;
 import com.teamwizardry.librarianlib.client.sprite.Sprite;
 import com.teamwizardry.librarianlib.client.sprite.Texture;
+import com.teamwizardry.librarianlib.common.util.math.Vec2d;
 import com.teamwizardry.refraction.Refraction;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
@@ -24,96 +24,118 @@ import java.util.ArrayList;
  */
 public class GuiBook extends GuiBase {
 
-	public static final Texture BACKGROUND_TEXTURE = new Texture(new ResourceLocation(Refraction.MOD_ID, "textures/gui/book_background.png"));
-	public static final Sprite BACKGROUND_SPRITE = BACKGROUND_TEXTURE.getSprite("bg", 256, 256);
-	public static final Texture BACKGROUND_HANDLE_TEXTURE = new Texture(new ResourceLocation(Refraction.MOD_ID, "textures/gui/book_background_handle.png"));
-	public static final Sprite BACKGROUND_HANDLE_SPRITE = BACKGROUND_HANDLE_TEXTURE.getSprite("bg", 256, 256);
-	static final int iconSize = 12;
-	@NotNull
-	public static SidebarItem selectedSiderbar;
-	public static ComponentText textComponent;
-	public static ArrayList<SidebarItem> categories = new ArrayList<>();
-	private int currentPage = 0;
+    public static final Texture SPRITE_SHEET = new Texture(new ResourceLocation(Refraction.MOD_ID, "textures/gui/sprite_sheet.png"));
+    public static final Sprite SCROLL_SLIDER_VERTICAL = SPRITE_SHEET.getSprite("scroll_slider_v", 8, 16);
+    public static final Sprite SCROLL_SLIDER_HORIZONTAL = SPRITE_SHEET.getSprite("scroll_slider_h", 16, 8);
+    public static final Sprite SCROLL_GROOVE_VERTICAL_MIDDLE = SPRITE_SHEET.getSprite("scroll_groove_v", 12, 12);
+    public static final Sprite SCROLL_GROOVE_VERTICAL_TOP = SPRITE_SHEET.getSprite("scroll_groove_v_top", 12, 12);
+    public static final Sprite SCROLL_GROOVE_VERTICAL_BOTTOM = SPRITE_SHEET.getSprite("scroll_groove_v_bottom", 12, 12);
 
-	public GuiBook() {
-		super(256, 256);
+    public static final Texture BACKGROUND_TEXTURE = new Texture(new ResourceLocation(Refraction.MOD_ID, "textures/gui/book_background.png"));
+    public static final Sprite BACKGROUND_SPRITE = BACKGROUND_TEXTURE.getSprite("bg", 256, 256);
+    public static final Texture BACKGROUND_HANDLE_TEXTURE = new Texture(new ResourceLocation(Refraction.MOD_ID, "textures/gui/book_background_handle.png"));
+    public static final Sprite BACKGROUND_HANDLE_SPRITE = BACKGROUND_HANDLE_TEXTURE.getSprite("bg", 256, 256);
+    static final int iconSize = 12;
+    @NotNull
+    public static SidebarItem selectedSiderbar;
+    public static ComponentText textComponent;
+    public static ArrayList<SidebarItem> categories = new ArrayList<>();
+    private int currentPage = 0;
 
-		textComponent = new ComponentText(16, 16, ComponentText.TextAlignH.LEFT, ComponentText.TextAlignV.TOP);
-		textComponent.getWrap().setValue(230);
+    public GuiBook() {
+        super(256, 256);
 
-		int id = 0;
+        int width = 210, height = 225;
+        ComponentVoid parent = new ComponentVoid(20, 10, width, height);
 
-		String langname = Minecraft.getMinecraft().getLanguageManager().getCurrentLanguage().getLanguageCode();
-		InputStream stream;
-		try {
-			stream = LibrarianLib.PROXY.getResource(Refraction.MOD_ID, "tablet/" + langname + ".json");
-		} catch (Throwable e) {
-			stream = LibrarianLib.PROXY.getResource(Refraction.MOD_ID, "tablet/en_US.json");
-		}
-		if (stream != null) {
-			InputStreamReader reader = new InputStreamReader(stream);
-			JsonElement json = new JsonParser().parse(reader);
-			if (json.isJsonObject() && json.getAsJsonObject().has("pages")) {
-				JsonArray array = json.getAsJsonObject().getAsJsonArray("pages");
+        ComponentScrolledView scrolledView = new ComponentScrolledView(0, 0, width, height);
+        parent.add(scrolledView);
 
-				if (array.isJsonArray()) {
-					for (int i = 0; i < array.size(); i++) {
-						if (array.get(i).isJsonObject()) {
-							JsonObject object = array.get(i).getAsJsonObject();
-							if (object.has("info") && object.has("subpages") && object.has("icon")) {
-								String info = object.get("info").getAsString();
-								ResourceLocation icon = new ResourceLocation(object.get("icon").getAsString());
-								ResourceLocation iconQualified = new ResourceLocation(icon.getResourceDomain(), "textures/"+icon.getResourcePath()+".png");
+        ComponentSpriteCapped scrollGrooves = new ComponentSpriteCapped(SCROLL_GROOVE_VERTICAL_TOP, SCROLL_GROOVE_VERTICAL_MIDDLE, SCROLL_GROOVE_VERTICAL_BOTTOM, false, width, 0, 12, height);
+        parent.add(scrollGrooves);
 
-								SidebarItem item = new SidebarItem(id++, new Sprite(iconQualified), info);
+        ComponentSlider scrollSlider = new ComponentSlider(6, SCROLL_SLIDER_VERTICAL.getHeight() / 2, 0, height - SCROLL_SLIDER_VERTICAL.getHeight(), 0, height);
+        scrollSlider.getHandle().add(new ComponentSprite(SCROLL_SLIDER_VERTICAL, -SCROLL_SLIDER_VERTICAL.getWidth() / 2, -SCROLL_SLIDER_VERTICAL.getHeight() / 2));
+        scrollSlider.getPercentageChange().add((p) -> scrolledView.scrollToPercent(new Vec2d(0, p)));
+        scrollGrooves.add(scrollSlider);
 
-								JsonArray pages = object.get("subpages").getAsJsonArray();
-								if (pages.isJsonArray()) {
-									ArrayList<SubPageItem> subPages = new ArrayList<>();
+        textComponent = new ComponentText(0, 0, ComponentText.TextAlignH.LEFT, ComponentText.TextAlignV.TOP);
+        textComponent.getWrap().setValue(width - 5);
+        scrolledView.add(textComponent);
 
-									int subID = 0;
-									for (JsonElement element : pages) {
-										if (element.isJsonObject() && element.getAsJsonObject().has("info") && element.getAsJsonObject().has("text")) {
-											JsonObject page = element.getAsJsonObject();
-											if (page.get("info").isJsonPrimitive() && page.get("text").isJsonArray()) {
-												String pageInfo = page.get("info").getAsString();
-												JsonArray pageArray = page.getAsJsonArray("text");
-												SubPageItem pageItem = new SubPageItem(item, subID++, pageInfo, pageArray);
-												subPages.add(pageItem);
-											}
-										}
-									}
+        int id = 0;
 
-									item.setPages(subPages);
-									item.prevMillis = System.currentTimeMillis();
-									categories.add(item);
-									if (i == 0) selectedSiderbar = item;
-									getMainComponents().add(item.get());
-								}
-							}
-						}
-					}
-				}
-			}
-		}
+        String langname = Minecraft.getMinecraft().getLanguageManager().getCurrentLanguage().getLanguageCode();
+        InputStream stream;
+        try {
+            stream = LibrarianLib.PROXY.getResource(Refraction.MOD_ID, "tablet/" + langname + ".json");
+        } catch (Throwable e) {
+            stream = LibrarianLib.PROXY.getResource(Refraction.MOD_ID, "tablet/en_US.json");
+        }
+        if (stream != null) {
+            InputStreamReader reader = new InputStreamReader(stream);
+            JsonElement json = new JsonParser().parse(reader);
+            if (json.isJsonObject() && json.getAsJsonObject().has("pages")) {
+                JsonArray array = json.getAsJsonObject().getAsJsonArray("pages");
 
-		ComponentSprite background = new ComponentSprite(BACKGROUND_SPRITE,
-				(getGuiWidth() / 2) - (BACKGROUND_SPRITE.getWidth() / 2),
-				(getGuiHeight() / 2) - (BACKGROUND_SPRITE.getHeight() / 2));
-		getMainComponents().add(background);
+                if (array.isJsonArray()) {
+                    for (int i = 0; i < array.size(); i++) {
+                        if (array.get(i).isJsonObject()) {
+                            JsonObject object = array.get(i).getAsJsonObject();
+                            if (object.has("info") && object.has("subpages") && object.has("icon")) {
+                                String info = object.get("info").getAsString();
+                                ResourceLocation icon = new ResourceLocation(object.get("icon").getAsString());
+                                ResourceLocation iconQualified = new ResourceLocation(icon.getResourceDomain(), "textures/" + icon.getResourcePath() + ".png");
 
-		ComponentSprite background_handle = new ComponentSprite(BACKGROUND_HANDLE_SPRITE,
-				(getGuiWidth() / 2) - (BACKGROUND_HANDLE_SPRITE.getWidth() / 2),
-				(getGuiHeight() / 2) - (BACKGROUND_HANDLE_SPRITE.getHeight() / 2));
-		getMainComponents().add(background_handle);
+                                SidebarItem item = new SidebarItem(id++, new Sprite(iconQualified), info);
 
-		textComponent.getUnicode().setValue(true);
-		textComponent.getText().setValue(".");
-		getMainComponents().add(textComponent);
-	}
+                                JsonArray pages = object.get("subpages").getAsJsonArray();
+                                if (pages.isJsonArray()) {
+                                    ArrayList<SubPageItem> subPages = new ArrayList<>();
 
-	@Override
-	public boolean doesGuiPauseGame() {
-		return true;
-	}
+                                    int subID = 0;
+                                    for (JsonElement element : pages) {
+                                        if (element.isJsonObject() && element.getAsJsonObject().has("info") && element.getAsJsonObject().has("text")) {
+                                            JsonObject page = element.getAsJsonObject();
+                                            if (page.get("info").isJsonPrimitive() && page.get("text").isJsonArray()) {
+                                                String pageInfo = page.get("info").getAsString();
+                                                JsonArray pageArray = page.getAsJsonArray("text");
+                                                SubPageItem pageItem = new SubPageItem(item, subID++, pageInfo, pageArray);
+                                                subPages.add(pageItem);
+                                            }
+                                        }
+                                    }
+
+                                    item.setPages(subPages);
+                                    item.prevMillis = System.currentTimeMillis();
+                                    categories.add(item);
+                                    if (i == 0) selectedSiderbar = item;
+                                    getMainComponents().add(item.get());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        ComponentSprite background = new ComponentSprite(BACKGROUND_SPRITE,
+                (getGuiWidth() / 2) - (BACKGROUND_SPRITE.getWidth() / 2),
+                (getGuiHeight() / 2) - (BACKGROUND_SPRITE.getHeight() / 2));
+        getMainComponents().add(background);
+
+        ComponentSprite background_handle = new ComponentSprite(BACKGROUND_HANDLE_SPRITE,
+                (getGuiWidth() / 2) - (BACKGROUND_HANDLE_SPRITE.getWidth() / 2),
+                (getGuiHeight() / 2) - (BACKGROUND_HANDLE_SPRITE.getHeight() / 2));
+        getMainComponents().add(background_handle);
+
+        textComponent.getUnicode().setValue(true);
+        textComponent.getText().setValue("potato");
+        getMainComponents().add(parent);
+    }
+
+    @Override
+    public boolean doesGuiPauseGame() {
+        return true;
+    }
 }
--- src/main/java/com/teamwizardry/refraction/client/gui/SubPageItem.java
+++ src/main/java/com/teamwizardry/refraction/client/gui/SubPageItem.java
@@ -21,78 +21,78 @@ import net.minecraft.util.text.TextFormatting;
  */
 public class SubPageItem {
 
-	private static final double TRANSITION_TIME_X = 0.75;
-	private final String info;
-	private final JsonArray text;
-	public float prevX = 0, destX, currentX;
-	public boolean isAnimating = false;
-	private SidebarItem sidebarItem;
-	private int id;
-	private ResourceLocation sliderLoc = new ResourceLocation(Refraction.MOD_ID, "textures/gui/slider_1.png");
-	private Texture sliderTexture = new Texture(sliderLoc);
-	private Sprite sliderSprite = sliderTexture.getSprite("slider", 130, 18);
+    private static final double TRANSITION_TIME_X = 0.75;
+    private final String info;
+    private final JsonArray text;
+    public float prevX = 0, destX, currentX;
+    public boolean isAnimating = false;
+    private SidebarItem sidebarItem;
+    private int id;
+    private ResourceLocation sliderLoc = new ResourceLocation(Refraction.MOD_ID, "textures/gui/slider_1.png");
+    private Texture sliderTexture = new Texture(sliderLoc);
+    private Sprite sliderSprite = sliderTexture.getSprite("slider", 130, 18);
 
-	public SubPageItem(SidebarItem sidebarItem, int id, String info, JsonArray text) {
-		this.sidebarItem = sidebarItem;
-		this.id = id;
-		this.info = info;
-		this.text = text;
-	}
+    public SubPageItem(SidebarItem sidebarItem, int id, String info, JsonArray text) {
+        this.sidebarItem = sidebarItem;
+        this.id = id;
+        this.info = info;
+        this.text = text;
+    }
 
-	public ComponentSprite get() {
-		ComponentSprite background = new ComponentSprite(sliderSprite, 20, 20 + 20 * id, 105, 18);
-		background.addTag(id);
+    public ComponentSprite get() {
+        ComponentSprite background = new ComponentSprite(sliderSprite, 20, 20 + 20 * id, 105, 18);
+        background.addTag(id);
 
-		ComponentText infoComp = new ComponentText(10, 8, ComponentText.TextAlignH.LEFT, ComponentText.TextAlignV.MIDDLE);
-		background.add(infoComp);
+        ComponentText infoComp = new ComponentText(10, 8, ComponentText.TextAlignH.LEFT, ComponentText.TextAlignV.MIDDLE);
+        background.add(infoComp);
 
-		new ButtonMixin<>(background, () -> {
-		});
+        new ButtonMixin<>(background, () -> {
+        });
 
-		background.BUS.hook(GuiComponent.ComponentTickEvent.class, (event) -> {
-			if (GuiBook.selectedSiderbar.getId() == sidebarItem.getId()) {
-				destX = 15;
-			} else destX = 10;
-			if (GuiBook.selectedSiderbar.getId() == sidebarItem.getId()) {
-				background.setVisible(true);
-				background.setEnabled(true);
+        background.BUS.hook(GuiComponent.ComponentTickEvent.class, (event) -> {
+            if (GuiBook.selectedSiderbar.getId() == sidebarItem.getId()) {
+                destX = 15;
+            } else destX = 10;
+            if (GuiBook.selectedSiderbar.getId() == sidebarItem.getId()) {
+                background.setVisible(true);
+                background.setEnabled(true);
 
-				double millisTransition = (System.currentTimeMillis() - sidebarItem.prevMillis) / 1000.0;
-				if (millisTransition < TRANSITION_TIME_X)
-					currentX = -MathHelper.sin((float) (millisTransition * Math.PI / (TRANSITION_TIME_X * 2))) * (destX - prevX) - prevX;
+                double millisTransition = (System.currentTimeMillis() - sidebarItem.prevMillis) / 1000.0;
+                if (millisTransition < TRANSITION_TIME_X)
+                    currentX = -MathHelper.sin((float) (millisTransition * Math.PI / (TRANSITION_TIME_X * 2))) * (destX - prevX) - prevX;
 
-				if (sidebarItem.currentPage == id) {
-					background.setPos(new Vec2d(10, 20 + 20 * id));
+                if (sidebarItem.currentPage == id) {
+                    background.setPos(new Vec2d(10, 20 + 20 * id));
 
-					infoComp.getText().setValue(TextFormatting.ITALIC + info);
+                    infoComp.getText().setValue(TextFormatting.ITALIC + info);
 
-					ITextHolder holder = TextAdapter.adapt(text);
-					GuiBook.textComponent.getText().setValue(holder.getFormattedText());
-				} else {
-					background.setPos(new Vec2d(15, 20 + 20 * id));
-					//background.setSize(new Vec2d(100, 18));
+                    ITextHolder holder = TextAdapter.adapt(text);
+                    GuiBook.textComponent.getText().setValue(holder.getFormattedText());
+                } else {
+                    background.setPos(new Vec2d(15, 20 + 20 * id));
+                    //background.setSize(new Vec2d(100, 18));
 
-					infoComp.getText().setValue(info);
-				}
-			} else {
-				//background.setPos(new Vec2d(-40, 20 + 20 * id));
-				//background.setSize(new Vec2d(100, 18));
-				background.setVisible(false);
-				background.setEnabled(false);
-			}
-		});
+                    infoComp.getText().setValue(info);
+                }
+            } else {
+                //background.setPos(new Vec2d(-40, 20 + 20 * id));
+                //background.setSize(new Vec2d(100, 18));
+                background.setVisible(false);
+                background.setEnabled(false);
+            }
+        });
 
-		background.BUS.hook(ButtonMixin.ButtonClickEvent.class, (event -> {
-			if (GuiBook.selectedSiderbar.getId() == sidebarItem.getId())
-				if (background.getEnabled() && background.isVisible() && event.getButton() == EnumMouseButton.LEFT)
-					if (sidebarItem.currentPage != id)
-						sidebarItem.currentPage = id;
-		}));
+        background.BUS.hook(ButtonMixin.ButtonClickEvent.class, (event -> {
+            if (GuiBook.selectedSiderbar.getId() == sidebarItem.getId())
+                if (background.getEnabled() && background.isVisible() && event.getButton() == EnumMouseButton.LEFT)
+                    if (sidebarItem.currentPage != id)
+                        sidebarItem.currentPage = id;
+        }));
 
-		return background;
-	}
+        return background;
+    }
 
-	public String getInfo() {
-		return info;
-	}
+    public String getInfo() {
+        return info;
+    }
 }
