db04c8739a011b655ff5f8566c97aacb94f70750
Saad <saadodi44@gmail.com>
2016-12-14 21:11:22 +0200

    add lang name for book

--- src/main/java/com/teamwizardry/refraction/common/item/ItemBook.java
+++ src/main/java/com/teamwizardry/refraction/common/item/ItemBook.java
@@ -1,13 +1,17 @@
 package com.teamwizardry.refraction.common.item;
 
+import com.teamwizardry.librarianlib.client.util.TooltipHelper;
 import com.teamwizardry.librarianlib.common.base.item.ItemMod;
 import com.teamwizardry.refraction.Refraction;
+import com.teamwizardry.refraction.api.Constants;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 /**
  * Created by Saad on 10/7/2016.
@@ -16,6 +20,11 @@ public class ItemBook extends ItemMod {
 
 	public ItemBook() {
 		super("book");
+	}
+
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer player, List<String> tooltip, boolean advanced) {
+		TooltipHelper.addToTooltip(tooltip, "simple_name." + Constants.MOD_ID + ":" + getRegistryName().getResourcePath());
 	}
 
 	@Override
