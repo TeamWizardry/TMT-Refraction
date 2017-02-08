eecc9b5006b4c9b60f14cbae64a99e07f8777a16
Saad <saadodi44@gmail.com>
2017-02-08 20:39:59 +0200

    ---- 1.3.4 ----

--- src/main/java/com/teamwizardry/refraction/api/Constants.java
+++ src/main/java/com/teamwizardry/refraction/api/Constants.java
@@ -7,7 +7,7 @@ public final class Constants {
 
 	public static final String MOD_ID = "refraction";
 	public static final String MOD_NAME = "Refraction";
-	public static final String VERSION = "1.3.3";
+	public static final String VERSION = "1.3.4";
 	public static final String CLIENT = "com.teamwizardry.refraction.client.proxy.ClientProxy";
 	public static final String SERVER = "com.teamwizardry.refraction.common.proxy.CommonProxy";
 	public static final String DEPENDENCIES = "required-after:librarianlib";
